a56e10cf99a063edbbcc84275d76e5519bc64fdb
ariel <ariel@arielherself.xyz>
2023-01-30 23:45:38 +0800

    fix: typo

--- dbio.py
+++ dbio.py
@@ -151,7 +151,7 @@ def markToDo(uid: any, lN: any) -> any:
             ls = lN.split('&')
             result = 0
             for each in ls:
-                result += delToDo(uid, each)
+                result += markToDo(uid, each)
         else:
             toDoList: list = readAll(uid)
             assert not -1 in toDoList
